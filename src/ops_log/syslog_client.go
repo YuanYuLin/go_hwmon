5ca88f69cd71107bb56043baa329cd69105e24c4
ylin14 <ylin14@YLIN14-PC0DD0KN.lenovo.com>
2019-10-25 17:31:11 +0800

    MAJOR_NUM=1 MINOR_NUM=4 AUX_NUM=0 Refactoring, supported Eclipse.

--- src/ops_log/syslog_client.go
+++ src/ops_log/syslog_client.go
@@ -1,32 +1,38 @@
 package ops_log
 
 import "fmt"
-import "log/syslog"
+//import "log/syslog"
 
 func Info(mask uint32, format string, a ...interface{}) {
 	log_str := fmt.Sprintf(format, a)
 	fmt.Println(log_str)
 	return
+	/*
 	log, _ := syslog.Dial("", "", syslog.LOG_USER|syslog.LOG_INFO, "iopc_go")
 	log.Info(log_str)
 	log.Close()
+	*/
 }
 
 func Debug(mask uint32, format string, a ...interface{}) {
 	log_str := fmt.Sprintf(format, a)
 	fmt.Println(log_str)
 	return
+	/*
 	log, _ := syslog.Dial("", "", syslog.LOG_USER|syslog.LOG_DEBUG, "iopc_go")
 	log.Debug(log_str)
 	log.Close()
+	*/
 }
 
 func Error(mask uint32, format string, a ...interface{}) {
 	log_str := fmt.Sprintf(format, a)
 	fmt.Println(log_str)
 	return
+	/*
 	log, _ := syslog.Dial("", "", syslog.LOG_USER|syslog.LOG_ERR, "iopc_go")
 	log.Err(log_str)
 	log.Close()
+	*/
 }
 
