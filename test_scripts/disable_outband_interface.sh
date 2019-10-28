299ea10ff3f45cd428ab28258c961827de2c3757
ytsai2 <ytsai2@YTSAI2-PF1LT7NJ.lenovo.com>
2019-10-28 15:29:18 +0800

    Deleted test shell script.

--- test_scripts/disable_outband_interface.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SRV_HOST="127.0.0.1:8088"
-# CPU Relative Temperature
-DATA='{"entity":0, "instant":0}'
-curl -d "$DATA" -H "Content-Type: application/json" -X POST http://$SRV_HOST/api/v1/hwmon/disable/out/interface
-echo ""
-
--- test_scripts/exit_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#curl -d $DATA -H "Content-Type: application/json" -X POST http://127.0.0.1:8080/api/v1/hwmon/gettemperature?
-DATA='{"entity":0, "instant":0}'
-curl -d "$DATA" -H "Content-Type: application/json" -X POST http://127.0.0.1:8080/api/v1/hwmon/exit/main
-echo ""
-
